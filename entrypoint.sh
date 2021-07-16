db769abbc59f731ac0ba5b888a90120e2c4d43f1
Apinan Woratrakun <iamapinan@gmail.com>
2021-07-16 21:39:40 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ mc mirror --overwrite $1 "deploy/$2"
 echo "URL to download."
 url=$(mc share download deploy/$2/app-release.apk)
 echo $url
-curl -X POST --data-urlencode "payload={\"channel\": \"#general\", \"username\": \"webhookbot\", \"text\": \"ไฟล์ API สำหรับทดสอบ $url\", \"icon_emoji\": \":rocket:\"}" $SLACK_HOOK
+curl -X POST --data-urlencode "payload={\"channel\": \"#general\", \"username\": \"GTMobile\", \"text\": \"ไฟล์ APK สำหรับทดสอบ $url\", \"icon_emoji\": \":rocket:\"}" $SLACK_HOOK
