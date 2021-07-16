6d46951227f269c23da76914054cf5ab3a0149f2
Apinan Woratrakun <iamapinan@gmail.com>
2021-07-16 19:37:17 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,7 @@ mc alias set deploy $MINIO_ENDPOINT $MINIO_ACCESS_KEY $MINIO_SECRET_KEY --api S3
 echo "deploy to: $1 deploy/$2"
 ls $1
 mc mirror --overwrite $1 "deploy/$2"
-echo "URL to download"
-mc share download deploy/$2/app-release.apk
+echo "URL to download."
+url=(mc share download deploy/$2/app-release.apk)
 
-
-curl -X POST --data-urlencode "payload={\"channel\": \"#general\", \"username\": \"webhookbot\", \"text\": \"ไฟล์ API สำหรับทดสอบ.\", \"icon_emoji\": \":rocket:\"}" https://hooks.slack.com/services/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
+curl -X POST --data-urlencode "payload={\"channel\": \"#general\", \"username\": \"webhookbot\", \"text\": \"ไฟล์ API สำหรับทดสอบ $url\", \"icon_emoji\": \":rocket:\"}" https://hooks.slack.com/services/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
