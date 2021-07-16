b8db95800565e3a17e765095e8105383de88bab4
Apinan Woratrakun <iamapinan@gmail.com>
2021-07-16 19:25:00 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,6 @@ ls $1
 mc mirror --overwrite $1 "deploy/$2"
 echo "URL to download"
 mc share download deploy/$2/app-release.apk
+
+
+curl -X POST --data-urlencode "payload={\"channel\": \"#general\", \"username\": \"webhookbot\", \"text\": \"ไฟล์ API สำหรับทดสอบ.\", \"icon_emoji\": \":rocket:\"}" https://hooks.slack.com/services/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
