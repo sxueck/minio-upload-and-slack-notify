9befefdd67b7be4eea4310e0aa73897aa16cda98
sxueck <sxuecks@gmail.com>
2022-07-16 23:30:34 +0800

    feat: delete url

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,3 @@ mc alias set deploy $MINIO_ENDPOINT $MINIO_ACCESS_KEY $MINIO_SECRET_KEY --api S3
 echo "deploy to: $1 deploy/$2"
 ls $1
 mc mirror --overwrite $1 "deploy/$2"
-echo "URL to download."
-url=$(mc share download deploy/$2$FILE_TO_DOWNLOAD  --json | jq -r '.share')
-echo $url
-curl -X POST --data-urlencode "payload={\"channel\": \"#general\", \"username\": \"$BOT\", \"text\": \"$MSG $url\", \"icon_emoji\": \":rocket:\"}" $SLACK_HOOK
